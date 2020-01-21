32c6500a390d3eeefb454e356d07e2764850325a
nelt <nel@flexio.fr>
2020-01-21 11:17:36 +0100

    extracted every task expression evaluation to EveryDateTimeTaskSelector
    ref #10

--- poom-crons-domain/src/main/java/org/codingmatters/poom/crons/domain/selector/DateTimeTaskSelector.java
+++ poom-crons-domain/src/main/java/org/codingmatters/poom/crons/domain/selector/DateTimeTaskSelector.java
@@ -3,6 +3,7 @@ package org.codingmatters.poom.crons.domain.selector;
 import org.codingmatters.poom.crons.crontab.api.types.TaskSpec;
 import org.codingmatters.poom.crons.crontab.api.types.taskspec.scheduled.At;
 import org.codingmatters.poom.crons.crontab.api.types.taskspec.scheduled.Every;
+import org.codingmatters.poom.crons.domain.selector.expression.EveryDateTimeTaskSelector;
 import org.codingmatters.poom.services.logging.CategorizedLogger;
 
 import java.time.DayOfWeek;
@@ -15,6 +16,7 @@ public class DateTimeTaskSelector implements TaskSelector {
 
     private final LocalDateTime atTime;
     private final ChronoUnit precision;
+    private final EveryDateTimeTaskSelector everyTaskSelector;
 
     public DateTimeTaskSelector(LocalDateTime atTime) {
         this(atTime, ChronoUnit.MINUTES);
@@ -26,6 +28,8 @@ public class DateTimeTaskSelector implements TaskSelector {
         if(!Arrays.asList(ChronoUnit.MINUTES, ChronoUnit.SECONDS).contains(precision)) {
             throw new InstantiationError("precision : " + precision + " is not supported");
         }
+
+        this.everyTaskSelector = new EveryDateTimeTaskSelector(this.atTime, this.precision);
     }
 
     @Override
@@ -34,7 +38,7 @@ public class DateTimeTaskSelector implements TaskSelector {
             return this.selectableAt(spec.scheduled().at());
         }
         if(spec.opt().scheduled().every().isPresent()) {
-            return this.selectableEvery(spec.scheduled().every());
+            return this.everyTaskSelector.selectable(spec.scheduled().every());
         }
         return false;
     }
@@ -56,72 +60,6 @@ public class DateTimeTaskSelector implements TaskSelector {
             return false;
         }
         return true;
-    }
-
-    private boolean selectableEvery(Every every) {
-        LocalDateTime startingAt = this.atPrecision(every.startingAt());
-        if(every.opt().seconds().isPresent()) {
-            if(this.precision == ChronoUnit.MINUTES) {
-                log.warn("task spec on every x seconds cannot be executed while precision is " + this.precision);
-                return false;
-            }
-            return Math.abs(ChronoUnit.SECONDS.between(this.atTime, startingAt)) % every.seconds() == 0L;
-        }
-        if(every.opt().minutes().isPresent()) {
-            return this.secondMatches(startingAt) &&
-                    Math.abs(ChronoUnit.MINUTES.between(this.atTime, startingAt)) % every.minutes() == 0L;
-        }
-        if(every.opt().hours().isPresent()) {
-            return this.minuteMatches(startingAt) &&
-                    Math.abs(ChronoUnit.HOURS.between(this.atTime, startingAt)) % every.hours() == 0L;
-        }
-        if(every.opt().days().isPresent()) {
-            return this.hourMatches(startingAt) &&
-                    Math.abs(ChronoUnit.DAYS.between(this.atTime, startingAt)) % every.days() == 0L;
-        }
-        if(every.opt().months().isPresent()) {
-            return this.dayOfMonthMatches(startingAt) &&
-                    Math.abs(ChronoUnit.MONTHS.between(this.atTime, startingAt)) % every.months() == 0L;
-        }
-        if(every.opt().years().isPresent()) {
-            return this.monthMatches(startingAt) &&
-                    Math.abs(ChronoUnit.YEARS.between(this.atTime, startingAt)) % every.years() == 0L;
-        }
-
-        return false;
-    }
-
-    private boolean monthMatches(LocalDateTime startingAt) {
-        return this.dayOfMonthMatches(startingAt) && this.atTime.getMonthValue() == startingAt.getMonthValue();
-    }
-
-    private boolean dayOfMonthMatches(LocalDateTime startingAt) {
-        return this.hourMatches(startingAt) && this.atTime.getDayOfMonth() == startingAt.getDayOfMonth();
-    }
-
-    private boolean hourMatches(LocalDateTime startingAt) {
-        return this.minuteMatches(startingAt) && this.atTime.getHour() == startingAt.getHour();
-    }
-
-    private boolean minuteMatches(LocalDateTime startingAt) {
-        return this.secondMatches(startingAt) && this.atTime.getMinute() == startingAt.getMinute();
-    }
-
-    private boolean secondMatches(LocalDateTime startingAt) {
-        if(this.precision.equals(ChronoUnit.MINUTES)) {
-            return true;
-        }
-        return this.atTime.getSecond() == startingAt.getSecond();
-    }
-
-    private LocalDateTime atPrecision(LocalDateTime dt) {
-        if(this.precision.equals(ChronoUnit.MINUTES)) {
-            return dt.withNano(0).withSecond(0);
-        } else if(this.precision.equals(ChronoUnit.SECONDS)) {
-            return dt.withNano(0);
-        } else {
-            return dt;
-        }
     }
 
     static public boolean sameDayOfWeek(At.DayOfWeek atDay, DayOfWeek javaDay) {
